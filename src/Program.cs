0d9a69e43e8ca3a26f05478d2acd34b0b8a054bc
acongiu <acongiu@CRM-UC-3915>
2023-08-22 11:33:29 +0200

    ticket3

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+string? prenom;
+string? nom;
+
+Console.WriteLine("Programme de saisie d'un prénom et d'un nom d'une personne. ");
+Console.WriteLine("Saisir le prénom : ");
+prenom = Console.ReadLine();
+
+Console.WriteLine("Saisir le nom : ");
+nom = Console.ReadLine();
