c8f02ac4dd5b58d144f3534643572d1d333936cc
acongiu <acongiu@CRM-UC-3915>
2023-08-22 16:43:09 +0200

    ticket7

--- src/Program.cs
+++ src/Program.cs
@@ -5,20 +5,26 @@ Console.WriteLine("Programme Personne");
 //
 string? prenom;
 string? nom;
-
-Console.WriteLine("Programme de saisie d'un prénom et d'un nom d'une personne. ");
-Console.WriteLine("Saisir le prénom : ");
-prenom = Console.ReadLine();
-
-Console.WriteLine("Saisir le nom : ");
-nom = Console.ReadLine();
-
-Console.WriteLine("La personne s'appelle : " +  prenom +" "+ nom);
-
-Person myPerson = new Person(prenom,nom);
-
-Console.WriteLine("La personne s'appelle : " +  myPerson.Prenom +" "+ myPerson.Nom);
-
+Person myPerson; 
 PeopleContainer myPeopleContainer = new PeopleContainer();
 
-myPeopleContainer.AddPerson(myPerson);
+Console.WriteLine("Programme de saisie d'un prénom et d'un nom d'une personne. Pour commencer, appuyer sur entrée. ");
+
+while(Console.ReadLine() != "q")
+{
+    Console.WriteLine("Saisir le prénom : ");
+    prenom = Console.ReadLine();
+
+    Console.WriteLine("Saisir le nom : ");
+    nom = Console.ReadLine();
+
+    Console.WriteLine("La personne s'appelle : " + prenom + " " + nom);
+
+    myPerson = new Person(prenom, nom);
+
+    Console.WriteLine("La personne s'appelle : " +  myPerson.Prenom + " "+ myPerson.Nom);
+
+    myPeopleContainer.AddPerson(myPerson);
+
+    Console.WriteLine("Pour arrêter la saisie de personnes, saisir q : ");
+}
