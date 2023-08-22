c368e2e296a03d7bbe0144edcb32bd854b840dc2
acongiu <acongiu@CRM-UC-3915>
2023-08-22 11:17:08 +0200

    ticket2

---
+++ src/ClassesMetier/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.ClassesMetier
+{
+    internal class Person
+    {
+        private string prenom;
+        private string nom;
+        public Person(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+        }
+    }
+}
