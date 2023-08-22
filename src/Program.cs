6a6bf70e529808b1393153902fc2c0fe3971c964
Thomas Debray <thomas2bray@free.fr>
2023-08-22 12:41:48 +0200

    Affichage du prenom et nom de myPerson

--- src/Program.cs
+++ src/Program.cs
@@ -15,3 +15,5 @@ nom = Console.ReadLine();
 Console.WriteLine("La personne s'appelle : " +  prenom + nom);
 
 Person myPerson = new Person(prenom,nom);
+
+Console.WriteLine("La personne s'appelle : " +  myPerson.Prenom + myPerson.Nom);
